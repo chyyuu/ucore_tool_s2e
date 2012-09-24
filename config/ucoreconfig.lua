873ab3906006f4461f4163a26ee3edb19cb30588
fwl <fanwlexca@gmail.com>
2012-09-24 20:28:03 +0800

    add ucoreconfig.lua

---
+++ config/ucoreconfig.lua
@@ -0,0 +1,31 @@
+-- config.lua
+
+s2e = {
+   kleeArgs = {
+      -- Whatever options you like
+   }
+}
+
+plugins = {
+   "BaseInstructions",
+   "UCoreMonitor",
+   "UCoreMemoryManagement"
+}
+
+pluginsConfig = {
+}
+
+
+pluginsConfig.BaseInstructions = {
+}
+
+pluginsConfig.UCoreMonitor = {
+   kernelBase  = 0xc0100000,
+   kernelEnd = 0xc01000ff,
+   MonitorFunction = true,
+   system_map_file = "/home/fwl/s2e/build/lab8/obj/kernel.sym"
+}
+
+pluginsConfig.UCoreMemoryManagement = {
+   print_pgdir_pc = 0xc010008f
+}
